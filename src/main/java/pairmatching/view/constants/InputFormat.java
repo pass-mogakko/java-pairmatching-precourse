6d26579a4924cf087e7b684680f3369e04762b40
yoondgu <doyou2ing@naver.com>
2022-12-14 17:00:28 +0900

    refactor(ui-3): 과정, 레벨, 미션 입력 정규표현식 간소화

--- src/main/java/pairmatching/view/constants/InputFormat.java
+++ src/main/java/pairmatching/view/constants/InputFormat.java
@@ -5,5 +5,5 @@ import java.util.regex.Pattern;
 public class InputFormat {
     public static final String DELIMITER_STEP = ",";
     public static final Pattern PATTERN_STEP = Pattern.compile(
-            "^(([ㄱ-ㅎ|가-힣a-zA-Z0-9]+), ([ㄱ-ㅎ|가-힣a-zA-Z0-9]+), ([ㄱ-ㅎ|가-힣a-zA-Z0-9]+))$");
+            "^((\\S+), (\\S+), (\\S+))$");
 }
